45386f254dd84ebc5f637657517047bd09175314
ArtfulLee <aastepsb@yandex.ru>
2023-11-12 11:41:09 +0300

    feat: add burger menu

--- src/js/burger-menu/burger-menu.js
+++ src/js/burger-menu/burger-menu.js
@@ -1,1 +1,16 @@
 const burger = document.querySelector('.site-nav__button')
+const navBurger = document.querySelector('.site-nav')
+
+function burgerMenu() {
+  if (burger.classList.contains('site-nav__button-burger')) {
+    burger.classList.remove('site-nav__button-burger')
+    burger.classList.add('site-nav__button-cross')
+    navBurger.classList.replace('site-nav--closed', 'site-nav--opened')
+  } else {
+    burger.classList.remove('site-nav__button-cross')
+    burger.classList.add('site-nav__button-burger')
+    navBurger.classList.replace('site-nav--opened', 'site-nav--closed')
+  }
+}
+
+burger.addEventListener('click', burgerMenu)
