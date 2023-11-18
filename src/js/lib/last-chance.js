51afc39a61f814d2721fc8ff9cbdd4daace690b5
ArtfulLee <aastepsb@yandex.ru>
2023-11-18 12:53:12 +0300

    feat: last chance section WIP

--- src/js/lib/last-chance.js
+++ src/js/lib/last-chance.js
@@ -1,45 +1,32 @@
 'use strict'
 
 function lastChance() {
-  let mySwiper = ''
-  let breakpoint = window.matchMedia('(max-width: 767px)')
-  let breakpointChecker = function () {
-    if (breakpoint.matches) {
-      if (mySwiper) {
-        mySwiper.destroy(true, true)
-      }
-
-      mySwiper = new Swiper('.last-chance__slider', {
-        spaceBetween: 10,
-        pagination: {
-          el: '.last-chance__slider-pagination',
-          type: 'fraction',
-        },
-
-        scrollbar: {
-          el: '.last-chance__slider-scrollbar',
-        },
-      })
-      return
-    } else {
-      if (mySwiper) {
-        mySwiper.destroy(true, true)
-      }
-
-      mySwiper = new Swiper('.last-chance__slider', {
-        pagination: {
-          el: '.last-chance__slider-pagination',
-          type: 'bullets',
-          clickable: true,
-          renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + '0' + (index + 1) + '</span>'
-          },
-        },
-      })
-    }
-  }
-  breakpoint.addListener(breakpointChecker)
-  breakpointChecker()
+  var swiper = new Swiper('.last-chance__slider', {
+    slidesPerView: 1,
+    spaceBetween: 16,
+    pagination: {
+      el: '.last-chance__slider-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      639: {
+        slidesPerView: 2,
+        spaceBetween: 16,
+      },
+      767: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1023: {
+        slidesPerView: 4,
+        spaceBetween: 32,
+      },
+      1439: {
+        slidesPerView: 5,
+        spaceBetween: 32,
+      },
+    },
+  })
 }
 
 lastChance()
